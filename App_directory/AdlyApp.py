ad54fa5fed0608f9c197dbc70563345da433c028
sebcampos <sebcampos@criptext.com>
2021-06-11 14:13:39 -0700

    elipsis needs work

--- App_directory/AdlyApp.py
+++ App_directory/AdlyApp.py
@@ -18,12 +18,21 @@ Config.set("graphics", "resizable", True)
 class MainApp(App):
     #build function is conventional way to build the app
     def build(self):
-        #setting the title for the windo
+        #setting the title for the window
         self.title = "Adly Beta Version"
+        
         #setting the app window attribute to a float layout using the FloatLayout() class
         self.window = FloatLayout()
-        #adding mainTitle object to the app window attribute
+        
+        #adding mainTitle object to the app window 
         self.window.add_widget(mainTitle)
+        
+        #adding navBar object to the app window 
+        self.window.add_widget(navBar)
+        
+        
+        self.window.add_widget(three_dots)
+        
         #returning the window will return the app
         return self.window
 
--- App_directory/AdlyObjs.py
+++ App_directory/AdlyObjs.py
@@ -1,14 +1,50 @@
 #import kivy Objects/Classes to build our version of it
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
 
 
 #Main Title
 mainTitle = Label( 
     text = "[font=fonts/great-vibes/GreatVibes-Regular.otf]Adly[/font]",
-    font_size = '200',
-    pos_hint={'center_x': .2 , 'center_y': .90 },
-    size_hint = (.1, .1),
+    font_size = '100',
+    pos_hint={'center_x': .2 , 'center_y': .9},
+    #size_hint = (1, 1),
     markup = True
     )
-        
+
+#Navigation bar
+navBar = BoxLayout(
+    orientation = "horizontal",
+    size_hint=(.8, .1),
+    pos_hint={'center_x': .5, 'center_y': .1 }
+    )
+
+navBar.add_widget(
+    Button(text="First")
+    )
+
+navBar.add_widget(
+    Button(text="Second")
+    )
+
+navBar.add_widget(
+    Button(text="Third")
+    )
+
+navBar.add_widget(
+    Button(text="Fourth")
+    )
+
+#three dots
+three_dots = Button(
+    size_hint=(.05, .1),
+    pos_hint={'center_x': .9 , 'center_y': .9},
+    )
+    
+three_dots.background_normal = "./imgs/three_dots.jpg"
+
+
+#img = Image(source="./imgs/test.png")
