3b037ef10879632c3e913c5b9e5cdc2d38bc4d83
sebcampos <sebcampos@criptext.com>
2021-06-11 12:17:43 -0700

    cleaning up scripts

--- App_directory/AdlyApp.py
+++ App_directory/AdlyApp.py
@@ -6,7 +6,7 @@ from kivy.lang import Builder
 from kivy.config import Config
 
 
-#Our Class object for Adly
+#Our objects for Adly
 from AdlyObjs import *
 
 #Set out App to resizeable for Mobile functionality
@@ -14,14 +14,19 @@ Config.set("graphics", "resizable", True)
 
 
 
-
 #Main App Object
 class MainApp(App):
+    #build function is conventional way to build the app
     def build(self):
+        #setting the title for the windo
         self.title = "Adly Beta Version"
-        self.window = FloatLayout
+        #setting the app window attribute to a float layout using the FloatLayout() class
+        self.window = FloatLayout()
+        #adding mainTitle object to the app window attribute
         self.window.add_widget(mainTitle)
+        #returning the window will return the app
         return self.window
+
 
 if __name__ == "__main__":
     MainApp().run()
--- App_directory/AdlyObjs.py
+++ App_directory/AdlyObjs.py
@@ -1,19 +1,14 @@
-#import kivy Object to build our version of it
+#import kivy Objects/Classes to build our version of it
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
-
-
-#AdlyLayout will inherit from the FloatLayout object and will be our main Layout
-class AdlyLayout(FloatLayout):
-    pass
 
 
 #Main Title
 mainTitle = Label( 
     text = "[font=fonts/great-vibes/GreatVibes-Regular.otf]Adly[/font]",
-    font_size = '100',
+    font_size = '200',
     pos_hint={'center_x': .2 , 'center_y': .90 },
     size_hint = (.1, .1),
-    markup = True,
+    markup = True
     )
         
