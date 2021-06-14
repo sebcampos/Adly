f9b382b44e0ab36e36c57780ca632c238ed09f40
latinlightning <44148474+latinlightning@users.noreply.github.com>
2021-06-13 21:02:43 -0700

    Update and rename userClass.py to customerClass.py
    First attempt at writing python. This is the customer class. This class is used to initialize the customer values written above.

---
+++ App_directory/customerClass.py
@@ -0,0 +1,10 @@
+class customer: 
+    def __init__(userID, email, lastName, firstName, age, gender, birthDate, address, phone): 
+        customer.userID
+        customer.email
+        customer.lastName
+        customer.firstName
+        customer.age
+        customer.birthDate
+        customer.address
+        customer.phone
